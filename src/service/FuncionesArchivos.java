61a21b5270861f32d447f8541f5cf0c62847d18e
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2025-02-18 02:35:20 -0300

    Funcion de escribir archivo de Carta de Platos

--- src/service/FuncionesArchivos.java
+++ src/service/FuncionesArchivos.java
@@ -36,15 +36,16 @@ public class FuncionesArchivos {
         }
     }
 
-    public static void escribirArchivo(String rutaArchivo) {
+    public static void escribirArchivoMenu(String nuevoPlato) {
 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(RUTA_BASE_ARCHIVOS + rutaArchivo, true))) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(RUTA_BASE_ARCHIVOS + "carta_comidas.txt", true))) {
             // El segundo parámetro 'true' permite agregar al archivo en lugar de sobrescribir
-            bw.write("Esto es una línea añadida al archivo.");
-            bw.newLine();  // Añadir una nueva línea al archivo
-            bw.write("Otra línea de texto.");
+            // Añadir una nueva línea al archivo
+            bw.write(nuevoPlato);
+            bw.newLine();
         } catch (IOException e) {
             e.printStackTrace();
+            System.out.println("Error al escribir el archivo en " + RUTA_BASE_ARCHIVOS + "carta_comidas.txt");
         }
     }
 
@@ -75,51 +76,6 @@ public class FuncionesArchivos {
                     String[] partes = linea.split(";");
                     List<String> atributos = Arrays.asList(partes);
                     cartaComidas.add(parseComida(atributos));
-                    /* String nombre = partes[0];
-                    // Formateo de precio
-                    double precio = Double.parseDouble(partes[1]);
-                    // Formateo de disponibilidad
-                    boolean disponible = Boolean.parseBoolean(partes[2]);
-                    String descripcion = partes[3];
-                    // El quinto elemento indica el tipo de comida
-                    String tipo = partes[4];
-                    switch (tipo) {
-                        case "pizza":
-                            String queso = partes[5];
-                            String estilo = partes[6];
-                            boolean sinTacc = Boolean.parseBoolean(partes[7]);
-                            // Instanciamos un objeto de la clase Pizza y lo agregamos a la lista de la carta de comidas
-                            cartaComidas.add(new Pizza(nombre, precio, disponible, descripcion, tipo, queso, estilo, sinTacc));
-                            break;
-                        case "postre":
-                            sinTacc = Boolean.parseBoolean(partes[5]); // se definio otra variable con el mismo nombre
-                            boolean paraCompartir = Boolean.parseBoolean(partes[6]);
-                            // Instanciamos un objeto de la clase Postre y lo agregamos a la lista de la carta de comidas
-                            cartaComidas.add(new Postre(nombre, precio, disponible, descripcion, tipo, sinTacc, paraCompartir));
-                            break;
-                        case "hamburguesa":
-                            int cantPatys = Integer.parseInt(partes[5]);
-                            boolean veggie = Boolean.parseBoolean(partes[6]);
-                            String tipoPan = partes[7];
-                            String salsa = partes[8];
-                            List condimentos = List.of(partes[9]);
-                            // Instanciamos un objeto de la clase Hamburguesa y lo agregamos a la lista de la carta de comidas
-                            cartaComidas.add(new Hamburguesa(nombre, precio, disponible, descripcion, tipo, cantPatys, veggie, tipoPan, salsa, condimentos));
-                            break;
-                        case "pasta":
-                            salsa = partes[5];
-                            boolean casero = Boolean.parseBoolean(partes[6]);
-                            // Instanciamos un objeto de la clase Pasta y lo agregamos a la lista de la carta de comidas
-                            cartaComidas.add(new Pasta(nombre, precio, disponible, descripcion, tipo, salsa, casero));
-                            break;
-                        case "milanesa":
-                            List tamanios = List.of(partes[5]);
-                            // Instanciamos un objeto de la clase Milanesa y lo agregamos a la lista de la carta de comidas
-                            cartaComidas.add(new Milanesa(nombre, precio, disponible, descripcion, tipo, tamanios));
-                            break;
-                        default:
-                            System.out.println("Error leyendo el tipo de comida.");
-                    } */
                 }
             } catch (IOException e) {
                 e.printStackTrace();
